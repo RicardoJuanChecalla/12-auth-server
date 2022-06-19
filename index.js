92b6ac2ca64665e5874775bf3d2ff436427eca0e
Ricardo Checalla Velasquez <ricardie_7@hotmail.com>
2022-06-19 15:50:38 -0500

    changes in backend to heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("./db/config");
+const path = require('path');
 require('dotenv').config(); 
 
 //crear el servidor de aplicacion express
@@ -20,6 +21,11 @@ app.use(express.json());
 
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+//Manejar demas rutas ...
+app.get('*', ( req, res ) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+})
 
 app.listen( process.env.PORT, ()=>{
     console.log(`servidor corriendo en el puerto ${ process.env.PORT }`);
